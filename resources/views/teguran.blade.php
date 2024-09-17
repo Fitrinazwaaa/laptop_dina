3ae140a3eed123559ce2f600a4f6784290bb85e2
unknown <fitrinaz.27@example.com>
2024-09-17 21:18:11 +0700

    teguran lisan

--- resources/views/teguran.blade.php
+++ resources/views/teguran.blade.php
@@ -7,11 +7,13 @@
     <link rel="stylesheet" href="{{ asset('css/style-teguran.css') }}">
 </head>
 <body>
-
+    
+<h2 class="text-center">TEGURAN LISAN</h2>
+<h4 class="text-center">Poin >= 12</h4>
     <div class="card pink">
         <div class="left">
             <h3>Nazla Agustina</h3>
-            <hr>
+            <hr class="pink-hr"> <!-- Garis khusus untuk pink -->
             <p>222310277</p>
         </div>
         <div class="right">
@@ -24,7 +26,7 @@
     <div class="card blue">
         <div class="left">
             <h3>Pudin Agustus</h3>
-            <hr>
+            <hr class="blue-hr"> <!-- Garis khusus untuk blue -->
             <p>222310289</p>
         </div>
         <div class="right">
@@ -36,16 +38,30 @@
 
     <div class="card blue">
         <div class="left">
-            <h3>Ikbal Nur Ahmad</h3>
-            <hr>
-            <p>222310989</p>
+            <h3>Ikbal Nur Agus</h3>
+            <hr class="blue-hr"> <!-- Garis khusus untuk blue -->
+            <p>222310277</p>
         </div>
         <div class="right">
             <p>12 RPL 2</p>
             <br>
-            <p>Poin 14</p>
+            <p>Poin 15</p>
         </div>
     </div>
+
+    <div class="card pink">
+        <div class="left">
+            <h3>Tina Mahmudii</h3>
+            <hr class="pink-hr"> <!-- Garis khusus untuk pink -->
+            <p>232410298</p>
+        </div>
+        <div class="right">
+            <p>10 MP 2</p>
+            <br>
+            <p>Poin 12</p>
+        </div>
+    </div>
+    
 
 </body>
 </html>
